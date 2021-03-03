6ef08f70b99a928bf4af7080a741ecd6781111e9
Zak Stone <zrlstone@gmail.com>
2021-03-03 11:07:23 +0000

    added hamburger animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("hello zak");
+const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+
+btnHamburger.addEventListener('click', function(){
+  console.log('click hamburger');
+
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  }
+  else { //Open Hamburger Men
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+  }
+
+});
